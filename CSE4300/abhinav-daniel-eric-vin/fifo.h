5e5568fa59af5303cb0ed6eb718dd17addafe274
Daniel B <danbok2016@outlook.com>
2025-11-04 20:14:46 -0500

    renamed fifo function params

--- CSE4300/abhinav-daniel-eric-vin/fifo.h
+++ CSE4300/abhinav-daniel-eric-vin/fifo.h
@@ -1,6 +1,6 @@
 #ifndef FIFO_H
 #define FIFO_H
 
-void fifo(struct process **procArray, int procArraySize, int globalTime);
+void fifo(struct process **FIFOQueue, int queueSize, int globalTime);
 
 #endif
